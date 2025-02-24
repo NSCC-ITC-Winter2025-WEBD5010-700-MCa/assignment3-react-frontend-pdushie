b95c863606df5f0ce121bf4a1c25d7102a9bd08a
Philip Dushie <philip.dushie@gmail.com>
2025-02-24 15:24:12 -0400

    seperated book form into separate component

--- src/components/books/BookEdit.jsx
+++ src/components/books/BookEdit.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useParams, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { useQuery, useMutation, useQueryClient, QueryClient } from "@tanstack/react-query";
+import BookForm from "./BookForm";
 
 function BookEdit() {
   const  { id } = useParams();
   const { register, handleSubmit, formState: {errors}, setValue } =  useForm();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const {isPending, error, data} = useQuery({
     queryKey: ['books', id],
@@ -15,6 +18,23 @@ function BookEdit() {
     },
     onSuccess: (bookData) => {
       console.log(bookData)
+    }
+  });
+
+  const editBookMutation = useMutation({
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3000/books/${id}`,
+        {
+          method: 'PUT',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(data)
+        }
+      )
+      return response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['booksData']);
+      navigate('/admin/books');
     }
   })
 
@@ -28,28 +48,14 @@ function BookEdit() {
       setValue('genre', data.genre)
     }
   }, [data])
+  const processData = (data) => {
+    editBookMutation.mutate(data);
+  }
+
   return (
       <div>
         <h2 className="text-2xl mb-5">Editing Book with ID: {data?.id}</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-5">
-          <input {...register('title', {required: 'Title is required'})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Title"></input>
-          {errors.title && <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>}
-          </div>
-          <div className="mb-5">
-          <input {...register('author', {required: true})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Author"></input>
-          {errors.author && <p className="text-red-500 text-sm mt-1">{errors.title.message}</p>}
-          </div>
-          <div className="mb-5">
-          <input {...register('published_year', {required: true, valueAsNumber: true, min: {value: 1700, message: 'Year must be greater than 1700'}})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Year"></input>
-          </div>
-          <div className="mb-5">
-          <input {...register('genre', {required: true})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Genre"></input>
-          </div>
-          <div className="mb-5">
-          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">Update Book</button>
-          </div>
-      </form>
+        <BookForm />
       </div>
       
      );
--- src/components/vehicle/EditVehicleManufacturer.jsx
+++ src/components/vehicle/EditVehicleManufacturer.jsx
@@ -6,8 +6,8 @@ import { useForm } from "react-hook-form";
 function EditVehicleManufacturer() {
 
   const { id } = useParams();
-  const { setValue } = useForm();
-    const { isPending, error, data, status } = useQuery({
+  const { handleSubmit, register, setValue } = useForm();
+  const { isPending, error, data, status } = useQuery({
     queryKey: ['vehicleManufacturerData', id],
     queryFn: async () => {
       console.log("Fetching vehicle manufacturer data for edit...")
@@ -16,47 +16,45 @@ function EditVehicleManufacturer() {
           method: 'GET'
         }
       );
-      console.log(response)
-      console.log(isPending)
+
       return response.json();
     }
   });
 
   useEffect(() => {
-    if(data){
+    if (data) {
       setValue('brand_name', data.brand_name)
-      setValue('brand_name', data.company_name)
-      setValue('brand_name', data.year_founded)
-      setValue('brand_name', data.founder)
-      setValue('brand_name', data.current_ceo)
+      setValue('company_name', data.company_name)
+      setValue('year_founded', data.year_founded)
+      setValue('founder', data.founder)
+      setValue('current_ceo', data.current_ceo)
     }
     console.log('Fetched data', data)
   }, [data])
 
   return (
     <>
-      <div>{data && data.brand_name}</div>
       <h1 className="text-2xl mb-5">Editng Vehicle Manufacturer with ID: {id}</h1>
       <form >
         <div className="mb-5">
-          <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Brand Name"></input>
-        </div>
-        <div className="mb-5">
-          <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Company Name"></input>
-        </div>
-        <div className="mb-5">
-          <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Year Founded"></input>
-        </div>
-        <div className="mb-5">
-          <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Founder"></input>
-        </div>
-        <div className="mb-5">
-          <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Current CEO"></input>
-        </div>
-        <div className="mb-5">
-          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">Update Vehicle Manufacturer</button>
-        </div>
-      </form>
+          <input {...register('brand_name')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Brand Name"></input>
+      </div>
+      <div className="mb-5">
+        <input {...register('company_name')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Company Name"></input>
+      </div>
+      <div className="mb-5">
+        <input {...register('year_founded')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Year Founded"></input>
+      </div>
+      <div className="mb-5">
+        <input {...register('founder')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Founder"></input>
+      </div>
+      <div className="mb-5">
+        <input {...register('current_ceo')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2xl p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Current CEO"></input>
+      </div>
+      <div className="mb-5">
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">Update Vehicle Manufacturer</button>
+      </div>
+    </form >
     </>
 
 
